0ab1b962c3035153d31528a21dd0d97ccea1cf72
Josimar Ferreira <josimarferreira922@yahoo.com.br>
2022-02-27 11:00:21 -0300

    Criando botão para navegar para a criação de novos produtos

--- crud/frontend/src/app/components/product/product-create/product-create.component.ts
+++ crud/frontend/src/app/components/product/product-create/product-create.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-create.component.css']
 })
 export class ProductCreateComponent implements OnInit {
+  propLegal = 'qualquer'
 
   constructor() { }
 
