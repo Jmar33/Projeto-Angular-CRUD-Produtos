46eb137dae86144debeb18773c7d0d22f5418926
Josimar Ferreira <josimarferreira922@yahoo.com.br>
2022-03-01 00:48:24 -0300

    Criando componente e método para listar os produtos do backend

--- crud/frontend/src/app/app.module.ts
+++ crud/frontend/src/app/app.module.ts
@@ -24,6 +24,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { FormsModule }  from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { ProductReadComponent } from './components/product/product-read/product-read.component';
 
 @NgModule({
   declarations: [
@@ -36,6 +37,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
     RedDirective,
     ForDirective,
     ProductCreateComponent,
+    ProductReadComponent,
   ],
   imports: [
     BrowserModule,
